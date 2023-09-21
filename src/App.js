8776717ec3b6aa3bbca347a954189f0dc96f1624
IG <iikim2511@gmail.com>
2023-09-21 00:15:01 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React from 'react'
-import './App.css';
-
 // 🦄전체 scss 한개파일에 몰아서 정리하기 (공부용이니까)
 import './App.scss';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
 import ProductList_with_api from './fetchData/ProductList_with_api';
 import ProductList_without_api from './fetchData/ProductList_without_api';
 
-const App = () => {
 
-  
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Products from './pages/Products';
+import Product from './pages/Product';
+import Home from './pages/Home';
 
-  
 
+
+// 🍀Layout
+const Layout = () => {
   return (
-    <div>App
- 
-    {/* <ProductList_with_api/> */}
-    {/* <ProductList_without_api/> */}
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
+// 🍀router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // 🍉children
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} /> 
     </div>
   )
 }
