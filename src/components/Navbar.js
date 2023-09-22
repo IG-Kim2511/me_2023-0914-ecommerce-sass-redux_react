ed7b991f38ed00536649750b614b76c257bdaad9
IG <iikim2511@gmail.com>
2023-09-22 02:20:48 -0400

    navbar-selected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -14,45 +14,75 @@ const Navbar = () => {
     //   const [open,setOpen] = useState(false)
     //   const products = useSelector((state) => state.cart.products);
 
+    const [selectedCategory, setSelectedCategory] = useState(''); // State to track the selected category
+
+    const handleCategoryChange = (event) => {
+      setSelectedCategory(event.target.value); // Update the selected category when the dropdown value changes
+    };
+
+
   return (
     <div id='navbar'>
         <div className="wrapper">
             <section className="left">
                 <div className="item">
-                    {/* 🦄public 폴더안 img가져옴 , src="/img/~~~ "*/}
-                    <img src='/img/en.png'/>
-                    <ExpandMoreIcon/>
+                    {/* 🦄public 폴더안 img가져옴 , src="/img/~~~ " */}
+                    <img src='/img/en.png'/>                    
                 </div>
                 <div className="item">
-                    <span>USD</span>
-                    <ExpandMoreIcon/>
+                { <Link className='link' to="/">Store</Link>}                    
+                </div> 
+
+               {/*  <div className="item">
+                    <Link className ="link" to="/products/1">smartphones</Link>
                 </div>
                 <div className="item">
-                    <Link className ="link" to="/products/1">Women</Link>
+                    <Link className ="link" to="/products/2">laptops</Link>
                 </div>
                 <div className="item">
-                    <Link className ="link" to="/products/2">Men</Link>
+                    <Link className ="link" to="/products/3">fragrances</Link>
                 </div>
                 <div className="item">
-                    <Link className ="link" to="/products/3">Children</Link>
+                    <Link className ="link" to="/products/3">skincare</Link>
                 </div>
+                <div className="item">
+                    <Link className ="link" to="/products/3">groceries</Link>
+                </div>
+                <div className="item">
+                    <Link className ="link" to="/products/3">home-decoration</Link>
+                </div> */}
+
+
+                <select value={selectedCategory} onChange={handleCategoryChange}>
+                    <option value="">Select a category</option>
+                    <option value="smartphones">Smartphones</option>
+                    <option value="laptops">Laptops</option>
+                    <option value="fragrances">Fragrances</option>
+                    <option value="skincare">Skincare</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="home-decoration">Home Decoration</option>
+                </select>
+
+                {/* Display the selected category as a link */}
+                {selectedCategory && (
+                    <div className="item">
+                        <Link className="link" to={`/products/${selectedCategory}`}>
+                            {selectedCategory}
+                        </Link>
+                    </div>
+                )}
             </section>
+
+            {/*   
             <section className="center">
-               { <Link className='link' to="/">Store</Link>}
-            </section>
+                { <Link className='link' to="/">Store</Link>}
+                </section> 
+            */}
+
             <section className="right">
                 <div className="item">
-                    <Link className ="link" to="/">Homepage</Link>
-                </div>
-                <div className="item">
-                    <Link className ="link" to="/">About</Link>
-                </div>
-                <div className="item">
-                    <Link className ="link" to="/">Contact</Link>
-                </div>
-                <div className="item">
-                    <Link className ="link" to="/">Stores</Link>
-                </div>
+                    <Link className ="link" to="/">Home</Link>
+                </div>       
                 <div className="icons">
                     <SearchIcon/>
                     <PersonIcon/>
