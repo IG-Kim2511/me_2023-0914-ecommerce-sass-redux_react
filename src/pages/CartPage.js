be7712df8b4464829a2d1789e33c4a566bc88c66
IG <iikim2511@gmail.com>
2023-09-25 01:35:43 -0400

    --ing

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,29 +1,60 @@
 import React from 'react'
 
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart } from '../redux/actions/cartActions';
+
 
 const CartPage = () => {
+
+    const cartItems = useSelector((state)=> state.cart.cartItems);
+    
+    const dispatch = useDispatch();
+
+    const handleRemoveFromCart = (productId) => {
+        dispatch(removeFromCart(productId));
+      };
+
   return (
     <div id='CartPage'>
-        <h1>Cart</h1>
-        {/* product?.map */}
-        <section className="item">
-            <img/>
-            <div className="details">
-                <h1>title</h1>
-                <p>desc</p>
-                <div className="price">price</div>
-                <DeleteForeverIcon/>
-            </div>
-            <div className="total">
-                <span>subtotal</span>
-                <span>subtotal price</span>                
-            </div>
-            <button>checkout</button>
-            <span className="reset">reset cart</span>        
-        </section>
+        <h1>Cart</h1>    
+        <ul className="items">
+        {cartItems.map((item) => {
+          
+            return (
+                <ul className="items">
+                {cartItems.map((item)=>(
+                    <li key={item.productId}>
+                    <img src={item.thumbnail} alt={item.title} />
+                    <div className="details">
+                        <h1>{item.title}</h1>
+                        <p>{item.description}</p>
+                        <div className="price">${item.price}</div>
+                        <button onClick={() => handleRemoveFromCart()}>
+                            <DeleteForeverIcon />
+                        </button>
+                    </div>
+                    <div className="total">
+                        <span>Subtotal</span>
+                        <span>${item.price * item.quantity}</span>
+                    </div>
+                </li>
+                ))}
+                
+                </ul>
+                
+            );
+        })}
+    </ul>
+
+        
     </div>
   )
 }
 
 export default CartPage
+
+
+
+
+
