2b45743ec3fa981a49236b2cec9bf85440fdcbbb
IG <iikim2511@gmail.com>
2023-09-25 02:27:50 -0400

    dispatch qty

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,60 +1,45 @@
-import React from 'react'
-
+import React from 'react';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeFromCart } from '../redux/actions/cartActions';
-
+// import { removeFromCart } from '../redux/actions/cartActions';
 
 const CartPage = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
 
-    const cartItems = useSelector((state)=> state.cart.cartItems);
-    
-    const dispatch = useDispatch();
-
-    const handleRemoveFromCart = (productId) => {
-        dispatch(removeFromCart(productId));
-      };
+  const handleRemoveFromCart = (productId) => {
+    // dispatch(removeFromCart(productId));   
+    dispatch({ type: "REMOVE_FROM_CART", payload: productId });
+  };
 
   return (
     <div id='CartPage'>
-        <h1>Cart</h1>    
-        <ul className="items">
-        {cartItems.map((item) => {
+      <h1>Cart</h1>
+      <ul className="items">      
+
+        {cartItems.map((item) => (
+            <li key={item.productId}>
+              {item.thumbnail && <img src={item.thumbnail} alt={item.title} />}
+              <div className="details">
+                {item.title && <h1>{item.title}</h1>}
+                {item.description && <p>{item.description}</p>}
+                {item.price && <div className="price">${item.price}</div>}
+                <button onClick={() => handleRemoveFromCart(item.id)}>
+                  <DeleteForeverIcon />
+                </button>
+                {item.quantity}
+              </div>
+              <div className="total">
+                <span>Subtotal</span>
+                {item.price && <span>${item.price * item.quantity}</span>}
+              </div>
+            </li>
+          ))}
           
-            return (
-                <ul className="items">
-                {cartItems.map((item)=>(
-                    <li key={item.productId}>
-                    <img src={item.thumbnail} alt={item.title} />
-                    <div className="details">
-                        <h1>{item.title}</h1>
-                        <p>{item.description}</p>
-                        <div className="price">${item.price}</div>
-                        <button onClick={() => handleRemoveFromCart(item.id)}>
-                            <DeleteForeverIcon />
-                        </button>
-                    </div>
-                    <div className="total">
-                        <span>Subtotal</span>
-                        <span>${item.price * item.quantity}</span>
-                    </div>
-                </li>
-                ))}
-                
-                </ul>
-                
-            );
-        })}
-    </ul>
 
-        
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default CartPage
-
-
-
-
-
+export default CartPage;
