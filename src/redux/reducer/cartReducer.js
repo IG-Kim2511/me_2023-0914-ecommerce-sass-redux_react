41eb3cd0a1e0887c73730f9562aab4bc5e554d58
IG <iikim2511@gmail.com>
2023-09-25 02:48:04 -0400

    cartreducer:;existingItem

--- src/redux/reducer/cartReducer.js
+++ src/redux/reducer/cartReducer.js
@@ -1,23 +1,42 @@
 // src/reducers/cartReducer.js
 const initialState = {
-    cartItems: [],
-  };
-  
-  const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TO_CART':
+  cartItems: [],
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.product.id
+      );
+
+      if (existingItem) {
+        // If the item already exists in the cart, increase its quantity
+        const updatedCartItems = state.cartItems.map((item) =>
+          item.id === action.product.id
+            ? { ...item, quantity: item.quantity + action.product.quantity }
+            : item
+        );
+        return {
+          ...state,
+          cartItems: updatedCartItems,
+        };
+      } else {
+        // If it's a new item, add it to the cart
         return {
           ...state,
           cartItems: [...state.cartItems, action.product],
         };
-      case 'REMOVE_FROM_CART':
-        return {
-          ...state,
-          cartItems: state.cartItems.filter((item) => item.id !== action.productId),
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default cartReducer;
+      }
+
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id !== action.productId),
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
