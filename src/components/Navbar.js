679ed522496d3571245a4fe7873976ee6b156c86
IG <iikim2511@gmail.com>
2023-09-26 01:39:39 -0400

    ---ing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ I wanna move page as soon as i click option
 
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Favorite } from '@mui/icons-material';
-import { useSelector } from 'react-redux';
 
 
 
--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar2.scss";
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar2 = () => {
+
+    // 🍀responsive nav bar
   const [click, setClicked] = useState(false);
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(true);
 
   const handleClick = () => {
     setClicked(!click);
@@ -16,42 +21,63 @@ const Navbar2 = () => {
     setShowNav(false);
   };
 
-  useEffect(() => {
-    document.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        setShowNav(true);
-      } else {
-        setShowNav(false);
-      }
-    });
-  }, []);
+//   useEffect(() => {
+//     document.addEventListener("scroll", () => {
+//       if (window.scrollY > 100) {
+//         setShowNav(true);
+//       } else {
+//         setShowNav(false);
+//       }
+//     });
+//   }, []);
+// ----end---
+
+  // 🍀useNavigate (= useHistory new version)
+  const navigate = useNavigate();
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    if (selectedCategory) {
+      navigate(`/products/${selectedCategory}`); // Use navigate to navigate
+    }
+  };
+  
+  const [selectedCategory, setSelectedCategory] = useState(''); 
+  // --end--
+
+  //   🍉redux    
+  const cartItems = useSelector((state) => state.cart.cartItems);  
 
   return (
+    // 🍉showNav?
     <nav className={showNav ? "navbar-container" : "navbar-container active"}>
-      <a href="/" className="navbar-logo">
-        "Logo"
-        <p>React Navbar</p>
-      </a>
+        <div className="item">
+            { <Link className='link' to="/">Store0914</Link>}                    
+        </div> 
+
+      {/* burger menu */}
       <div className="menu-icon" onClick={handleClick}>
         {click ? <CloseIcon/> : <MenuIcon/>}
       </div>
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-link-items" onClick={handleCloseMobileMenu}>
-          <a href="/" className="nav-link">
-            Home
-          </a>
-        </li>
-        <li className="nav-link-items" onClick={handleCloseMobileMenu}>
-          <a href="/" className="nav-link">
-            Products
-          </a>
-        </li>
-        <li className="nav-link-items" onClick={handleCloseMobileMenu}>
-          <a href="/" className="nav-link">
-            Contact
-          </a>
-        </li>
-      </ul>
+
+      <main className={click ? "nav-menu active" : "nav-menu"}>
+
+        <div className="nav-link-items" onClick={handleCloseMobileMenu}>
+        { <Link className='link' to="/all" >All Items</Link>}                    
+        </div> 
+        
+        {/* 👉useNavigate */}
+        <select className='nav-link-items' onChange={handleCategoryChange}>
+            <option value="">Select a category</option>
+            <option value="smartphones">Smartphones</option>
+            <option value="laptops">Laptops</option>
+            <option value="fragrances">Fragrances</option>
+            <option value="skincare">Skincare</option>
+            <option value="groceries">Groceries</option>
+            <option value="home-decoration">Home Decoration</option>
+        </select> 
+            
+      </main>
     </nav>
   );
 };
