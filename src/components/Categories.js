2a1697e2efb0020be4d7215d9ce98a0c2bfdaf15
IG <iikim2511@gmail.com>
2023-10-01 00:32:49 -0400

    category : fix

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -8,40 +8,32 @@ const Categories = () => {
       <section class="parent">
         <div class="div1">
          <img src="https://i.imgur.com/ngBbEN5.jpg" alt=""/>
-          <button>
           {/* 🍀/products/~~~ */}
-            <Link className="link" to="/products/groceries">groceries</Link>
-          </button>
+            <Link className="link" to="/products/groceries"><button className="myButton">
+            groceries</button></Link>
+     
         </div>
         <div class="div2">
           <img src="https://i.imgur.com/jgcx4vg.jpg" alt=""/>
-          <button>          
-            <Link className="link" to="/products/fragrances">fragrances</Link>
-          </button>
+          <Link className="link" to="/products/fragrances"><button className="myButton">fragrances</button></Link>
+        
         </div>
         <div class="div3">
           <img src="https://i.imgur.com/RdhpLtf.jpg" alt=""/>
-          <button>          
-            <Link className="link" to="/products/home-decoration">home-decoration</Link>
-          </button>
+          <Link className="link" to="/products/home-decoration"><button className="myButton">home-decoration</button></Link>
+          
         </div>
         <div class="div4">
           <img src="https://i.imgur.com/qEiQtfA.jpg" alt=""/>
-          <button>          
-            <Link className="link" to="/products/laptops">laptops</Link>
-          </button>
+          <Link className="link" to="/products/laptops"><button className="myButton">laptops</button></Link>          
         </div>
         <div class="div5">
           <img src="https://i.imgur.com/sVex7iS.jpg" alt=""/>
-          <button>          
-            <Link className="link" to="/products/skincare">skincare</Link>
-          </button>
+          <Link className="link" to="/products/skincare"><button className="myButton">skincare</button></Link>
         </div>
         <div class="div6">
           <img src="https://i.imgur.com/12aNmCU.jpg" alt=""/>
-          <button>          
-            <Link className="link" to="/products/smartphones">smartphones</Link>
-          </button>
+          <Link className="link" to="/products/smartphones"><button className="myButton">smartphones</button></Link>      
         </div>
       </section>
     </div>
