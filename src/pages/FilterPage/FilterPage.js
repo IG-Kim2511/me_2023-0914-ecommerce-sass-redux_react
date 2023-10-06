93c42c34854196414071979ecdc094ae426d9a92
IG <iikim2511@gmail.com>
2023-10-06 17:07:41 -0400

    S: FilterPge.js

--- src/pages/FilterPage/FilterPage.js
+++ src/pages/FilterPage/FilterPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useFilter } from '../../context/FilterContext';
 import products from '../../data' // Replace with your product data source
-import ProductCard from './ProductCard';
+import { Link } from 'react-router-dom';
 
 function FilterPage() {
   const {
@@ -23,16 +23,53 @@ function FilterPage() {
     ));
 
   return (
-    <div>
-      {/* Render filter inputs here */}
-      {/* For example, input radio, checkbox, range, search */}
+    <div id="FilterPage">      
+      <section className="wrapper">
+        {/*     
+          <main className="left">
+     
+          <Filter onFilter={onFilter} />      
+        </main> */}
+
+        <main className="right">
+        {/* 🦄*/}
+          {/* <ProductsFiltered filteredProducts={filteredProducts} /> */}
+          <div id="ProductsFiltered">
+            <h2>Filtered Products</h2>
       
-      {/* Render filtered products */}
-      <ul className="bottom">
-        {filteredProducts.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </ul>
+            <section>
+              {filteredProducts.length === 0 ? (
+                <p>No results</p>
+              ) : (
+                <ul className="bottom">
+                  {filteredProducts.map((item) => (
+
+                        // 🦄link to product.js 
+                      <Link className='link' to={`/product/${item.id}`} key={item.id}>
+          
+                          <div className="card">
+                              {/* 🦄q: hover하면 이미지가 바뀜.
+                                  컨테이너안에 이미지가 2개있어야 함 */}
+                              <section className='imageWrap'>
+                                  <img src={item.images[0]} className="mainImg" alt={item.title} />
+                                  <img src={item.images[2]} className="secondImg" alt={item.title} />
+                              </section>
+                              <section className='titleWrapper'>
+                              <h4 className='title'>{item.title} <span>(id:{item.id})</span></h4>
+                              <div className='prices'>
+                                  <h4>price : {item.price}</h4>
+                                  <h4 className="stock">(stock : {item.stock})</h4>
+                              </div>
+                              </section>
+                          </div>
+                      </Link>
+                  ))}   
+                </ul>
+              )}
+            </section>
+          </div>
+        </main>
+      </section>
     </div>
   );
 }
