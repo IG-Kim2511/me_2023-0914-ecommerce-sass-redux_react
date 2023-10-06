fe4f733980915e50a213d0a80d1743ddea40f921
IG <iikim2511@gmail.com>
2023-10-06 14:30:17 -0400

    --clear

--- src/App.js
+++ src/App.js
@@ -6,21 +6,10 @@ redering filtered product with input range, radio, checkbox.
 */
 
 /* 🎃-ing
-
-add qty on cartitem array on redux from product.js
-
-라마 -localstorage대신해서 사용하는 library 사용법 익힘
-localstorage
-
-filter page 리덕스로 다시 만들기
-
-product.js : click image on side image and show it on main img container
-
-constext api
-
-input search feature
-
- */
+  constext api
+  filter page 리덕스로 다시 만들기
+  input search feature
+*/
 
 import React from 'react';
 // 🦄전체 scss 한개파일에 몰아서 정리하기 (공부용이니까)
